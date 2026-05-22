69c7a528a8a112be6b85ab2fce1d66ce2892e6bd
rafainan31 <intinanzahwibisono@gmail.com>
2026-05-22 21:35:11 +0700

    perbaikan

--- Beckend_Invofest/src/index.ts
+++ Beckend_Invofest/src/index.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import cors from "cors";
-
 import eventRoute from "./routes/eventRoute.js";
 import categoryRoute from "./routes/categoryRoute.js";
 import pembicaraRoute from "./routes/pembicaraRoute.js";
+
 
 const app = express();
 const port = 3000;
@@ -11,20 +11,18 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-app.use("/speakers", pembicaraRoute);
-
-app.get("/", (req, res) => {
-  res.send("Server is running");
+app.get("/",(req, res) => {
+    res.send("Ambagar!");
 });
 
-// ROUTE EVENT
 app.use("/events", eventRoute);
-
-// ROUTE CATEGORY
 app.use("/categories", categoryRoute);
+app.use("/pembicara", pembicaraRoute);
+
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
+
 
 export default app;
