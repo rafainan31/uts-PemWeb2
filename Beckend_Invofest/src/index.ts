c83798955d98f42d82fda16818a73b87541e15ac
rafainan31 <intinanzahwibisono@gmail.com>
2026-05-23 15:23:20 +0700

    perbaikan lagi

--- Beckend_Invofest/src/index.ts
+++ Beckend_Invofest/src/index.ts
@@ -1,24 +1,28 @@
 import express from "express";
 import cors from "cors";
+import eventRoute from "./routes/eventRoute.js";
+import categoryRoute from "./routes/categoryRoute.js";
+import pembicaraRoute from "./routes/pembicaraRoute.js";
 
-import eventRoute from "../src/routes/eventRoute.js";
-import categoryRoute from "../src/routes/categoryRoute.js";
-import pembicaraRoute from "../src/routes/pembicaraRoute.js";
 
 const app = express();
+const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Backend berhasil jalan di Vercel!");
+app.get("/",(req, res) => {
+    res.send("Ambagar!");
 });
 
-// app.use("/events", eventRoute);
-// app.use("/categories", categoryRoute);
+app.use("/events", eventRoute);
+app.use("/categories", categoryRoute);
+app.use("/pembicara", pembicaraRoute);
 
-// // biar dua-duanya bisa dipakai
-// app.use("/speakers", pembicaraRoute);
-// app.use("/pembicara", pembicaraRoute);
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
 
 export default app;
