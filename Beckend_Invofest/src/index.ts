8c48f1a93d5c0df45f1c613d8dcddbbca36cdbec
rafainan31 <intinanzahwibisono@gmail.com>
2026-05-23 11:47:32 +0700

    penambahan folder api

--- Beckend_Invofest/src/index.ts
+++ Beckend_Invofest/src/index.ts
@@ -1,28 +1,29 @@
 import express from "express";
 import cors from "cors";
-import eventRoute from "./routes/eventRoute.js";
-import categoryRoute from "./routes/categoryRoute.js";
-import pembicaraRoute from "./routes/pembicaraRoute.js";
 
+import eventRoute from "../src/routes/eventRoute.js";
+import categoryRoute from "../src/routes/categoryRoute.js";
+import pembicaraRoute from "../src/routes/pembicaraRoute.js";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req, res) => {
-    res.send("Ambagar!");
+app.get("/", (req, res) => {
+  res.send("Backend berhasil jalan di Vercel!");
 });
 
 app.use("/events", eventRoute);
 app.use("/categories", categoryRoute);
 app.use("/pembicara", pembicaraRoute);
 
-
-app.listen(port, () => {
+// app.listen hanya untuk lokal, bukan untuk Vercel
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
-});
-
+  });
+}
 
 export default app;
